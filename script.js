cd9d9f64e8d2c79ad63b6c4f119072ce911b66ab
brinybee <nguyenngocchau241007@gmail.com>
2025-03-26 22:48:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function login()
   let p = password.value
   if (u == "minhanh" && p == "241007")
   {
-    window.open("gioithieu.html")
+    window.open("Giới thiệu.html")
     document.getElementById(complier).style.display="block";
   }
 }
